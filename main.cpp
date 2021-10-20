1f2ac6003e0421020a969bdbe5bf4a59705c74f4
Stefano Galatà <gaste96@hotmail.it>
2021-10-20 09:36:25 +0200

    Gold league achieved

--- main.cpp
+++ main.cpp
@@ -6,25 +6,65 @@
 
 using namespace std;
 
-struct CheckpointPosition
+struct FVector
 {
-    int checkpointX;
-    int checkpointY;
+    float X;
+    float Y;
+
+    void Normalize()
+    {
+        Y = X/Length();
+        Y = Y/Length();
+    }
+
+    float Length()
+    {
+       return sqrt((X*X)+(Y*Y));
+    }
+
+    FVector() : X(0), Y(0) {}
+    FVector(float x, float y) : X(x), Y(y) {}
+
+    bool operator==(const FVector otherVector)
+    {
+        return (this->X == otherVector.X && this->Y == otherVector.Y);
+    }
+
+    FVector& operator*(const float multiplier)
+    {
+       multiplier * this->X; 
+       multiplier * this->Y;
+       return *this;
+    }
+
+    FVector& operator-(FVector otherVector)
+    {
+        this->X-otherVector.X;
+        this->Y - otherVector.Y;
+        return *this;
+    }
+};
+
+struct CheckpointEntry
+{
+    FVector checkpointPosition;
 
     int distanceFromLastCheckpoint;
 
-    CheckpointPosition() : checkpointX(0), checkpointY(0), distanceFromLastCheckpoint(0){};
-    CheckpointPosition(int x, int y, int distance) : checkpointX(x), checkpointY(y), distanceFromLastCheckpoint(distance){};
+    CheckpointEntry() : checkpointPosition(FVector()), distanceFromLastCheckpoint(0){};
+    CheckpointEntry(FVector position, int distance) : checkpointPosition(position), distanceFromLastCheckpoint(distance){} 
+    CheckpointEntry(float x, float y, int distance) : checkpointPosition(FVector(x,y)), distanceFromLastCheckpoint(distance) {}
+    
 };
 
 // register a new checkpoint, returns true if the checkpoint was already registered
-bool RegisterCheckPoint(vector<CheckpointPosition>& checkpointList, int& posX, int& posY)
+bool RegisterCheckPoint(vector<CheckpointEntry>& checkpointList, FVector& checkpointPosition)
 {
     bool checkpointAlreadyRegistered = false;
-    for(CheckpointPosition iterator : checkpointList)
+    for(CheckpointEntry iterator : checkpointList)
     {
         // if we found that this checkpoint is already registered we can break the loop because we registered all checkpoints
-        if(iterator.checkpointX == posX && iterator.checkpointY == posY)
+        if(iterator.checkpointPosition == checkpointPosition)
         {
             checkpointAlreadyRegistered = true;
             break;
@@ -34,7 +74,7 @@ bool RegisterCheckPoint(vector<CheckpointPosition>& checkpointList, int& posX, i
     // if this is a new checkpoint then we can add it to the list
     if(!checkpointAlreadyRegistered)
     {
-        CheckpointPosition checkpointToAdd = CheckpointPosition(posX, posY, 0);
+        CheckpointEntry checkpointToAdd = CheckpointEntry(checkpointPosition, 0);
         // if it is the first checkpoint that we add
         if(checkpointList.size()==0)
         {
@@ -43,8 +83,8 @@ bool RegisterCheckPoint(vector<CheckpointPosition>& checkpointList, int& posX, i
         }
         else
         {
-            CheckpointPosition lastCheckpoint = checkpointList.back();
-            int distance = sqrt(pow(lastCheckpoint.checkpointX - posX,2) + pow(lastCheckpoint.checkpointY-posY,2));
+            CheckpointEntry lastCheckpoint = checkpointList.back();
+            int distance = sqrt(pow(lastCheckpoint.checkpointPosition.X - checkpointPosition.X,2) + pow(lastCheckpoint.checkpointPosition.Y-checkpointPosition.Y,2));
             checkpointToAdd.distanceFromLastCheckpoint = distance;
             checkpointList.push_back(checkpointToAdd);
         }
@@ -53,26 +93,24 @@ bool RegisterCheckPoint(vector<CheckpointPosition>& checkpointList, int& posX, i
     return checkpointAlreadyRegistered;
 }
 
-void PrintDebugCheckpointList(vector<CheckpointPosition>& checkpointList)
+void PrintDebugCheckpointList(vector<CheckpointEntry>& checkpointList)
 {
     for(int i = 0; i<checkpointList.size(); ++i)
     {
-        cerr << "checkpoint " + to_string(i+1) << " | x = " << checkpointList[i].checkpointX <<  " y = " << checkpointList[i].checkpointY << " d = " << checkpointList[i].distanceFromLastCheckpoint << endl;
+        cerr << "checkpoint " + to_string(i+1) << " | x = " << checkpointList[i].checkpointPosition.X <<  " y = " << checkpointList[i].checkpointPosition.Y << " d = " << checkpointList[i].distanceFromLastCheckpoint << endl;
     }
 }
 
-CheckpointPosition GetFurthestCheckpoint(vector<CheckpointPosition>& checkpointList)
+CheckpointEntry GetFurthestCheckpoint(vector<CheckpointEntry>& checkpointList)
 {
     if(checkpointList.size() <= 0)
     {
-        return CheckpointPosition();
+        return CheckpointEntry();
     }
-    CheckpointPosition furthestCheckpoint = checkpointList.at(0);
-    //cerr << "checkpoint is " << to_string(1) << " With distance: " << to_string(furthestCheckpoint.distanceFromLastCheckpoint) << endl;
+    CheckpointEntry furthestCheckpoint = checkpointList.at(0);
     int j = 0;
     for(int i = 1; i<checkpointList.size(); i++)
     {
-        //cerr << "checkpoint is " << to_string(i+1) << " With distance: " << to_string(checkpointList[i].distanceFromLastCheckpoint) << endl;
         if(checkpointList[i].distanceFromLastCheckpoint > furthestCheckpoint.distanceFromLastCheckpoint)
         {
             furthestCheckpoint = checkpointList[i];
@@ -81,124 +119,109 @@ CheckpointPosition GetFurthestCheckpoint(vector<CheckpointPosition>& checkpointL
         }
     }
 
-    //cerr << "furthestCheckpoint is " << to_string(j+1) << " With distance: " << to_string(furthestCheckpoint.distanceFromLastCheckpoint) << endl;
-   
     return furthestCheckpoint;
 }
+
 
 int main()
 {
     int availableBoosts = 1;
     bool allCheckpointRegistered = false;
 
-    vector<CheckpointPosition> registeredCheckpoints = {};
-    registeredCheckpoints.empty();
+    bool doOnce = true;
 
-    int firstCheckpointX;
-    int firstCheckpointY;
+    // List of checkpoints
+    vector<CheckpointEntry> registeredCheckpoints = {};
 
-    bool firstCheckpointAssigned = false;
-    bool firstFrameSkipped = true;
+    FVector firstCheckpoint;
+    FVector nextCheckpoint;
 
-    int lastPodPositionX = 0; 
-    int lastPodPositionY = 0;
+    FVector lastPodPosition;
+    int angleToStopFeedVelocity = 90;
+    int checkpointRadius = 600;
+
+    int brakeFactor = 2;
 
 
     // game loop
     while (1) {
-        int x;
-        int y;
-
-        //vector<int> myPodPosition = {x,y};
-        
-        int nextCheckpointX;
-        int nextCheckpointY;
-
-        //vector<int> nextCheckpointPosition = {nextCheckpointX, nextCheckpointY};
+        FVector podPosition;
 
         int nextCheckpointDist;
         int nextCheckpointAngle;
 
         bool shouldUseBoost = false;
 
-        // we need to skip the first frame because the checkpoint positions are wrong in that frame
-        if(!firstCheckpointAssigned && !firstFrameSkipped)
+        cin >> podPosition.X >> podPosition.Y >> nextCheckpoint.X >> nextCheckpoint.Y >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+
+        if(doOnce)
         {
-            firstCheckpointAssigned = true;
-            firstCheckpointX = nextCheckpointX;
-            firstCheckpointY = nextCheckpointY;
+            firstCheckpoint = nextCheckpoint;
+            doOnce = false;
         }
 
-        if(!firstFrameSkipped)
-        {
-            // register every checkpoint to see which one is the furtherst and use the boost later
-            RegisterCheckPoint(registeredCheckpoints, nextCheckpointX, nextCheckpointY);
-        }
-        firstFrameSkipped = false;
+
+        // register every checkpoint to see which one is the furtherst and use the boost later
+        RegisterCheckPoint(registeredCheckpoints, nextCheckpoint);
 
         // this means that we registered all checkpoints
-        if(!allCheckpointRegistered && firstCheckpointX == nextCheckpointX && firstCheckpointY == nextCheckpointY && registeredCheckpoints.size() > 2)
+        if(!allCheckpointRegistered && firstCheckpoint == nextCheckpoint && registeredCheckpoints.size() > 2)
         {
             // fill the distanceFromLastCheckpoint of the first checkpoint
-            CheckpointPosition lastCheckpoint = registeredCheckpoints.back();
-            int distance = sqrt(pow(lastCheckpoint.checkpointX - nextCheckpointX, 2) + pow(lastCheckpoint.checkpointY-nextCheckpointY,2));
+            CheckpointEntry lastCheckpoint = registeredCheckpoints.back();
+            int distance = sqrt(pow(lastCheckpoint.checkpointPosition.X - nextCheckpoint.X, 2) + pow(lastCheckpoint.checkpointPosition.Y-nextCheckpoint.Y,2));
             registeredCheckpoints[0].distanceFromLastCheckpoint = distance;
             allCheckpointRegistered = true;
         }
 
         if(allCheckpointRegistered)
         {
-            CheckpointPosition furthestCheckpoint = GetFurthestCheckpoint(registeredCheckpoints);
-            if(furthestCheckpoint.checkpointX == nextCheckpointX && furthestCheckpoint.checkpointY == nextCheckpointY)
+            CheckpointEntry furthestCheckpoint = GetFurthestCheckpoint(registeredCheckpoints);
+            if(furthestCheckpoint.checkpointPosition == nextCheckpoint)
             {
                 shouldUseBoost = true;
             }
         }
 
 
-        
-        cin >> x >> y >> nextCheckpointX >> nextCheckpointY >> nextCheckpointDist >> nextCheckpointAngle; cin.ignore();
+        FVector opponentPosition;
 
-        int opponentX;
-        int opponentY;
+        cin >> opponentPosition.X >> opponentPosition.Y; cin.ignore();
 
-        //vector<int> opponentPosition = {opponentX, opponentY};
+        int velocity;
 
-        cin >> opponentX >> opponentY; cin.ignore();
-
-        //int distanceFromOpponent = sqrt(pow(opponentX-x,2) + pow(opponentY-y,2));
-        int opponentDistanceFromLastCheckPoint = sqrt(pow(opponentX-nextCheckpointX,2) + pow(opponentY-nextCheckpointY,2));
-
-        int velocity = 100;
-
-        //if(nextCheckpointDist < 1500 &&)
-        
-        if(nextCheckpointAngle > 20 || nextCheckpointAngle < -20 || nextCheckpointDist<=3000)
+        if(nextCheckpointAngle > -angleToStopFeedVelocity && nextCheckpointAngle < angleToStopFeedVelocity)
         {
-            velocity = 100.f * (1.f-abs(((float)nextCheckpointAngle/90.f))) * ((float)nextCheckpointDist/(2.f*600.f)));
-            cerr << "Velocity calc = " + to_string(velocity) << endl;
+            velocity = 100;
+        }
+        else
+        {
+            velocity = 100.f * (1.f-(float)nextCheckpointAngle/90.f) * (float)(nextCheckpointDist/2.f*checkpointRadius);
+            cerr << "Velocity calc = " << to_string(velocity) << endl;
             velocity = clamp(velocity, 0, 100);
         }
-        
+
         
         string thrust = to_string(velocity);
-        
-        cerr << "Velocity = " + to_string(velocity) << endl;
-        cerr << "Next dist = " + to_string(nextCheckpointDist) << endl;
-        cerr << "Opponent Distance from checkPoint = " << opponentDistanceFromLastCheckPoint << endl;
-        cerr << "Next checkpoint = " + to_string(nextCheckpointX) << ", " << to_string(nextCheckpointY) << endl;
-        cerr << "Next checkpoint angle = " + to_string(nextCheckpointAngle) << endl;
-        cerr << "Cosine(nextCheckpointAngle) = " + to_string(cos(nextCheckpointAngle)) << endl;
-        cerr << "Available boosts = " + to_string(availableBoosts) << endl;
-        cerr << "All CheckpointRegistered = " + to_string(allCheckpointRegistered) << endl;
-        cerr << "Thrust = " + thrust << endl;
-        cerr << "Registered checkpoints = " + to_string(registeredCheckpoints.size()) << endl;
-        cerr << "Delta position = " << x-lastPodPositionX << ", " << y-lastPodPositionY << endl;
-        //cerr << "Angle between my and opp pods = " << angleBetweenMyPodAndOpponentPod << endl; 
-        //cerr << "Distance from opponent = " << distanceFromOpponent << endl;
 
-        lastPodPositionX = x;
-        lastPodPositionY = y;
+        FVector targetDestination;
+
+        targetDestination = nextCheckpoint-((podPosition-lastPodPosition) * brakeFactor);
+
+        cerr << "Velocity = " << velocity << endl;
+        cerr << "Next dist = " << nextCheckpointDist << endl;
+        cerr << "target pos = " << targetDestination.X << ", " << targetDestination.Y << endl;
+        cerr << "Next checkpoint = " << nextCheckpoint.X << ", " << nextCheckpoint.Y << endl;
+        cerr << "Next checkpoint Vec = " << nextCheckpoint.X << ", " << nextCheckpoint.Y << endl;
+        cerr << "Next checkpoint angle = " << nextCheckpointAngle << endl;
+        cerr << "Available boosts = " << availableBoosts << endl;
+        cerr << "Should use boost = " << shouldUseBoost << endl;
+        cerr << "All CheckpointRegistered = " << allCheckpointRegistered << endl;
+        cerr << "Thrust = " + thrust << endl;
+        cerr << "Registered checkpoints = " << registeredCheckpoints.size() << endl;
+        
+
+        lastPodPosition = podPosition;
         
 
         PrintDebugCheckpointList(registeredCheckpoints);
@@ -209,6 +232,6 @@ int main()
             thrust = "BOOST";
         }
 
-        cout << nextCheckpointX << " " << nextCheckpointY << " " + thrust << endl;
+        cout << targetDestination.X << " " << targetDestination.Y << " " + thrust << endl;
     }
 }
